23fc7561140f0d4f8f0298d58bb732886aa58d48
james0724 <jameskahoro07@gmail.com>
2021-05-20 23:07:42 +0300

    fixed 2

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const cookieParser = require('cookie-parser');
 
 const fileUpload = require('express-fileupload');
-//const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const path = require('path');
 
 const errorMiddleware = require('./middlewares/errors');
@@ -12,7 +12,7 @@ const errorMiddleware = require('./middlewares/errors');
 // Setting up config file
 if (process.env.NODE_ENV !== 'PRODUCTION')
 	require('dotenv').config({ path: 'backend/config/config.env' });
-//dotenv.config({ path: 'backend/config/config.env' });
+dotenv.config({ path: 'backend/config/config.env' });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
